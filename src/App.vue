a0db289f54e9e878ae6dbec5a8fd6924995c5f32
Spaghettimonstar <130066887+Spaghettimonstar@users.noreply.github.com>
2024-05-08 15:35:47 +0000

    added a box that opens upon button clicked, started to put video in.

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,30 @@
 
 <template>
 
-<h1 v-if=buttonClicked>information</h1>
+<h1 v-if=buttonClicked> <v-card elevation = "10"  title="The Scout" text="The scout is an offensive class
+  that focuses on his speed to flank enemies and attack them with his
+  scattergun, pistol or bat. 
+  However he is frail and can
+   only take 125 damage.
+   To counteract this
+   he can use his double jump
+   to evade bullets. He is the
+    fastest character in the game">
+  <v-img
+    class="bg-grey-lighten-2"
+    height="300"
+    src="https://tcrf.net/images/b/bc/TF2Final_ClassScoutRed.png"
+    contain
+    
+    ></v-img>
+  
+  <v-card-actions>
+    <v-btn> <iframe width="560" height="315" src="https://www.youtube.com/embed/IQw-4JABPCM?si=oRKBMiyxuOqVvWOp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> TO GO TO >SHARE AND CLICK EMBED, COPY THE LINK</v-btn>
+  </v-card-actions>
+</v-card></h1>
 <img src="src/assets/scoutiocn.png" width=100>
-<button @click="buttonClicked = !buttonClicked">kk</button>
+<v-btn icon=" src/assets/scoutiocn.png" @click="buttonClicked = !buttonClicked">kk</v-btn>
 
   
 </template>
+
