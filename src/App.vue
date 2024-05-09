c7ca0b7e62f8223d4a4b6b1c7c3c1df6ffe77284
danyooo <42193011+danyooo@users.noreply.github.com>
2024-05-09 00:36:01 +0000

    Create a button as an image

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
-
+<!-- <v-icon scouticon= "src/assets/scoutiocn.png"> </v-icon>
+ -->
 <script setup>  
-
   import { ref } from 'vue'
-  const test = ref(0)
+  // buttonclicked is false
   const buttonClicked = ref(false)
 </script>
-
+ 
 <template>
+  <!--create a button that when clicked, is not itself(kind of like a negative) -->
+  <!-- attach an icon to the button-->
+<v-btn icon @click="buttonClicked = !buttonClicked">
+  <!-- create the icon and set the image source to scouticon.png-->
+<v-icon>
+  <!--TODO: Find a way for the width to scale with button size( so no need to do it manually it just perfectly fits) -->
+ <img src= "@/assets/scouticon.png" width = 100 />
+</v-icon>
+</v-btn>
+  <!-- if the button is clicked..-->
+<p v-if=buttonClicked> 
+  <!--display text -->
+sup brother </p>
 
-<h1 v-if=buttonClicked>information</h1>
-<img src="src/assets/scoutiocn.png" width=100>
-<button @click="buttonClicked = !buttonClicked">kk</button>
 
   
 </template>
